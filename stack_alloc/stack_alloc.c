e32b95947391226d23b6e9075a4e5eb2eaa45f93
bug-py <cocorigolo6@gmail.com>
2026-07-22 00:24:24 +0200

    STACK RESIZE

--- stack_alloc/stack_alloc.c
+++ stack_alloc/stack_alloc.c
@@ -20,7 +20,7 @@ void stack_init(stack_t* stack,void* buffer,size_t length){
     stack->buffer=buffer;
     stack->length=length;
     stack->current_offset=0;
-    stack->prev_offset=0;
+    stack->last_alloc_offset=0;
 }
 void* stack_alloc(stack_t* stack,size_t size){
     void* ptr=stack->buffer+stack->current_offset;
@@ -33,25 +33,33 @@ void* stack_alloc(stack_t* stack,size_t size){
     void* header_ptr=stack->buffer+alloc_offset-sizeof(header_t);
     header_t header;
 
-    header.prev_offset=stack->prev_offset;
+    header.prev_alloc_offset=stack->last_alloc_offset;
     header.padding=padding;
-    header.alloc_size=size;
     memcpy(header_ptr,&header,sizeof(header_t));
  
 
-    stack->prev_offset=alloc_offset;
+    stack->last_alloc_offset=alloc_offset;
     stack->current_offset=new_offset;
     void* new_ptr=stack->buffer+alloc_offset;
     return new_ptr;
 }
+void* stack_resize(stack_t* stack,void* old_ptr,size_t new_size){
+    void* last_alloc_ptr=stack->buffer+stack->last_alloc_offset;
+    if(last_alloc_ptr!=old_ptr) return NULL;
+    size_t new_offset=stack->last_alloc_offset+new_size;
+    if(new_offset>stack->length) return NULL;
+    stack->current_offset=new_offset;
+    return last_alloc_ptr;
+    
+}
 int stack_free(stack_t* stack,void* old_ptr){
-    void* ptr=stack->buffer+stack->prev_offset;
-    if(ptr!=old_ptr) return -1;
-    void* header_ptr=(char*)ptr-sizeof(header_t);
+    void* last_alloc_ptr=stack->buffer+stack->last_alloc_offset;
+    if(last_alloc_ptr!=old_ptr) return -1;
+    void* header_ptr=(char*)last_alloc_ptr-sizeof(header_t);
     header_t header;
     memcpy(&header,header_ptr,sizeof(header_t));
-    stack->current_offset=stack->prev_offset-header.padding;
-    stack->prev_offset=header.prev_offset;
+    stack->current_offset=stack->last_alloc_offset-header.padding;
+    stack->last_alloc_offset=header.prev_alloc_offset;
     return 0;
 
 }
--- stack_alloc/stack_alloc.h
+++ stack_alloc/stack_alloc.h
@@ -6,15 +6,16 @@ typedef struct{
     char* buffer;
     size_t length;
     size_t current_offset;
-    size_t prev_offset;
+    size_t last_alloc_offset;
 }stack_t;
 
 typedef struct{
     uint8_t padding;
-    size_t alloc_size;
-    size_t prev_offset;
+    size_t prev_alloc_offset;
 }header_t;
 void stack_init(stack_t* stack,void* buffer,size_t length);
 void* stack_alloc(stack_t* stack,size_t size);
+void* stack_resize(stack_t* stack,void* old_ptr,size_t new_size);
 int stack_free(stack_t* stack,void* old_ptr);
+
 #endif
